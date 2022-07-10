47ad1cea9d77e1aaa4edc83d2f34d5a9669fc948
sysoev-kirill <sysoev.kirill@gmail.com>
2022-07-10 20:56:52 +0300

    Task_08

---
+++ task_08/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Enter a number: ");
+
+int num = Convert.ToInt32(Console.ReadLine());
+int res = 1;
+while (res < num)
+{
+   if (res % 2 == 0)
+   {
+      Console.WriteLine(res);
+   }
+   res++;
+}
